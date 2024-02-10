4e96ddc5e10ae46d0f26a34b230df7f1cff0f000
Mike A <git@mikealmel.ooo>
2024-02-10 22:15:41 +0100

    Make `errors.py` top-level

--- findmy/__init__.py
+++ findmy/__init__.py
@@ -1,8 +1,7 @@
 """A package providing everything you need to work with Apple's FindMy network."""
-from . import keys, reports, scanner
+from . import errors, keys, reports, scanner
 from .accessory import FindMyAccessory
 from .keys import KeyPair
-from .util import errors
 
 __all__ = (
     "keys",
