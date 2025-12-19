ab7f7255606762a677bcc7e2a6f0734654c0996c
Gabe Koleszar <gabriel.koleszar@uconn.edu>
2025-12-19 15:33:46 -0500

    Update to fallback to regex key parsing

--- findmy/plist.py
+++ findmy/plist.py
@@ -15,6 +15,7 @@ import plistlib
 import subprocess
 from pathlib import Path
 from typing import IO
+import re 
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
@@ -26,16 +27,33 @@ logger = logging.getLogger(__name__)
 _DEFAULT_SEARCH_PATH = Path.home() / "Library" / "com.apple.icloud.searchpartyd"
 
 
+
+def _parse_beaconstore_key_from_output(output: str) -> bytes:
+    if '"acct"<blob>="BeaconStoreKey"' not in output:
+        raise ValueError
+    m = re.search(r'"gena"<blob>=0x([0-9A-Fa-f]+)', output)
+    if not m:
+        raise ValueError
+    return bytes.fromhex(m.group(1))
+    
 # consider switching to this library https://github.com/microsoft/keyper
 # once they publish a version of it that includes my MR with the changes to make it compatible
 # with keys that are non-utf-8 encoded (like the BeaconStore one)
 # if I contribute this, properly escape the label argument here...
 def _get_beaconstore_key() -> bytes:
-    """Get the decryption key for BeaconStore using the system password prompt window."""
-    # This thing will pop up 2 Password Input windows...
-    key_in_hex = subprocess.getoutput("/usr/bin/security find-generic-password -l 'BeaconStore' -w")  # noqa: S605
-    return bytes.fromhex(key_in_hex)
-
+    try:
+        # This thing will pop up 2 Password Input windows...
+        key_in_hex = subprocess.getoutput(
+            "/usr/bin/security find-generic-password -l 'BeaconStore' -w"
+        )        
+        if not key_in_hex:
+            raise ValueError("Empty output from security -w")
+        return bytes.fromhex(key_in_hex)
+    except Exception:
+        output = subprocess.getoutput(
+            "/usr/bin/security find-generic-password -l 'BeaconStore'"
+        )
+        return _parse_beaconstore_key_from_output(output)
 
 def _get_accessory_name(
     accessory_id: str,
